469c5c56603cf4e2b81f417c8b3805e8733ea6ba
dmitryfedorov228 <116007725+dmitryfedorov228@users.noreply.github.com>
2023-03-29 14:49:41 +0600

    add files

---
+++ main.js
@@ -0,0 +1,39 @@
+//! task7
+
+// const arr = [2, 5, 9, 15, 0, 4]
+// for (let i = 0; i <= 10; i++){
+//    if(i > 3 && i < 10){
+//     console.log(i);
+//    }
+// }
+
+//! task8
+
+// let arr = [2, 5, 9, 15, 0, 4, -9];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0){
+//         sum += arr[i];
+//     }
+// }
+// console.log(sum);
+
+
+//! task9
+
+// const arr = [1, 2, 5, 9, 4, 13, 4, 10]
+
+// for (let i = 0; i < arr.length ; i ++) {
+//     if( arr [i] == 4)  {
+//             alert('есть')
+//             break;
+//     }
+// }
+
+//! task10
+
+// const arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log('-' + arr[i]);
+// }
