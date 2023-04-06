8d6bce0137672f4a04a8e027a7f13b6b1ff40b71
dmitryfedorov228 <116007725+dmitryfedorov228@users.noreply.github.com>
2023-04-06 17:03:54 +0600

    update

--- main.js
+++ main.js
@@ -1,4 +1,61 @@
-//! task7
+//! task1
+
+// for(let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+
+
+//! task2
+
+// for(let i = 11; i <= 33; i++){
+//     console.log(i);
+// }
+
+
+
+//! task3
+
+
+// for(let i = 0; i <= 100; i++) {
+//     if( i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+
+//! task4
+
+// let result = 0
+
+// for(let i = 1; i <= 100; i++){
+//     result += i
+// }
+// console.log(result);
+
+
+//! task5
+
+// const mas =  [1, 2, 3, 4, 5]
+
+// for(let i = 0; i < mas.length; i++){
+    
+// }
+// console.log(mas);
+
+
+//! task6
+
+// const arr = [1, 2, 3, 4, 5]
+
+// result = 0 
+
+// for(let i = 0; i <arr.length; i++) {
+//     result += arr[i]
+// }
+// console.log(result);
+
+ //! task7
 
 // const arr = [2, 5, 9, 15, 0, 4]
 // for (let i = 0; i <= 10; i++){
